183a1c8cea0b18acf41eee4d5cc5506d0edbfddf
yasminadelia <yasminadelia11@gmail.com>
2023-05-15 07:33:44 +0700

    add songs plugin in server, create table migrations

--- src/server.js
+++ src/server.js
@@ -6,8 +6,13 @@ const albums = require('./api/albums');
 const AlbumsService = require('./services/AlbumsService');
 const AlbumsValidator = require('./validator/albums');
 
+const songs = require('./api/songs');
+const SongsService = require('./services/SongsService');
+const SongsValidator = require('./validator/songs');
+
 const init = async () => {
   const albumsService = new AlbumsService();
+  const songsService = new SongsService();
 
   const server = Hapi.server({
     port: process.env.PORT,
@@ -20,13 +25,19 @@ const init = async () => {
   });
 
   // pasang plugin
-  await server.register({
+  await server.register([{
     plugin: albums,
     options: {
       service: albumsService,
       validator: AlbumsValidator,
     },
-  });
+  }, {
+    plugin: songs,
+    options: {
+      service: songsService,
+      validator: SongsValidator,
+    },
+  }]);
 
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
