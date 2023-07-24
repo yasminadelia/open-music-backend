0b5ebb5dd807e5250b72039200eddc02b17c3eea
yasminadelia <yasminadelia11@gmail.com>
2023-07-24 16:12:34 +0700

    adjustments on validator, SongsService, and songs handler

--- src/api/songs/handler.js
+++ src/api/songs/handler.js
@@ -16,12 +16,7 @@ class SongsHandler {
   async postSongHandler(request, h) {
     try {
       this._validator.validateSongPayload(request.payload);
-      const {
-        title, year, genre, performer, duration, albumId,
-      } = request.payload;
-      const songId = await this._service.addSong({
-        title, year, genre, performer, duration, albumId,
-      });
+      const songId = await this._service.addSong(request.payload);
 
       const response = h.response({
         status: 'success',
